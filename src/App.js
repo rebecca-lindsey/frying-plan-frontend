f477c872842a039f4521ada8049ec5d0e05d1469
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-15 13:32:24 -0400

    Set up React routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react';
-
-import HeaderContainer from './header/HeaderContainer'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import HeaderContainer from "./header/HeaderContainer";
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <HeaderContainer />
-      </div>
-    )
+      <Router>
+        <div className="App">
+          <HeaderContainer />
+        </div>
+        <Switch>
+          <Route path="/users/:id/days">
+            <h1>Days Component Here</h1>
+          </Route>
+          <Route path="/recipes">
+            <h1>Recipes Component Here</h1>
+          </Route>
+          <Route path="/recipes/new">
+            <h1>New Recipe Component Here</h1>
+          </Route>
+          <Route path="/">
+            <h1>Welcome Page</h1>
+          </Route>
+        </Switch>
+      </Router>
+    );
   }
 }
