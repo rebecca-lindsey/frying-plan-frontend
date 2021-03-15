2b1839506ada5c1689cdedf1a257e7e55f5be6ab
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-15 13:55:57 -0400

    Set up NavLinks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HeaderContainer from "./header/HeaderContainer";
 
 export default class App extends Component {
@@ -11,13 +11,13 @@ export default class App extends Component {
         </div>
         <Switch>
           <Route path="/users/:id/days">
-            <h1>Days Component Here</h1>
-          </Route>
-          <Route path="/recipes">
-            <h1>Recipes Component Here</h1>
+            <h1>Meal Plan Component Here</h1>
           </Route>
           <Route path="/recipes/new">
             <h1>New Recipe Component Here</h1>
+          </Route>
+          <Route path="/recipes">
+            <h1>Recipes Component Here</h1>
           </Route>
           <Route path="/">
             <h1>Welcome Page</h1>
