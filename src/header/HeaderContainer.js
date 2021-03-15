c14b0cb5a156852a6ae59fc29b6a9e2546fb4d54
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-15 13:55:57 -0400

    Set up NavLinks

--- src/header/HeaderContainer.js
+++ src/header/HeaderContainer.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Navigation from "./Navigation";
 
 export default class HeaderContainer extends Component {
   render() {
     return (
       <div>
         <h1>Frying Plan</h1>
-
+        <Navigation />
       </div>
     );
   }
--- src/header/Navigation.js
+++ src/header/Navigation.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 export default class Navigation extends Component {
   render() {
-    return <nav></nav>;
+    return (
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to={`/users/1/days`}>Meal Plan</NavLink>
+          </li>
+          <li>
+            <NavLink to="/recipes">All Recipes</NavLink>
+          </li>
+          <li>
+            <NavLink to="/recipes/new">New Recipe</NavLink>
+          </li>
+        </ul>
+      </nav>
+    );
   }
 }
