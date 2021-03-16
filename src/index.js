9db97e00653a4a0cfd9172b3071114b3d69c3a19
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-16 12:29:02 -0400

    Add thunk to index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
-import { Provider } from "react-redux"; /* code change */
-import recipeReducer from "./reducers/recipeReducer.js";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
+import recipeReducer from "./reducers/recipeReducer.js";
 import "./index.css";
 import App from "./App";
 
 const store = createStore(
   recipeReducer,
+  applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
