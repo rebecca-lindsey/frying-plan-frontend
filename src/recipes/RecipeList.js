3a963a8ff27ea25b5585356ecbfab77506b49f6a
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-16 15:03:59 -0400

    Set up RecipeCard

--- src/recipes/RecipeList.js
+++ src/recipes/RecipeList.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import RecipeCard from "./RecipeCard";
 
 class RecipeList extends Component {
+  makeRecipeCards() {
+    return this.props.recipes.map((recipe) => <RecipeCard recipe={recipe} />);
+  }
+
   render() {
-    return <div id="recipe-list"></div>;
+    return (
+      <div id="recipe-list">
+        <ul>{this.makeRecipeCards()}</ul>
+      </div>
+    );
   }
 }
 
-export default connect()(RecipeList);
+const mapStateToProps = (state) => {
+  return {
+    recipes: state.recipes,
+  };
+};
+
+export default connect(mapStateToProps)(RecipeList);
