d8e33faeaf7195c38743fdcd6e0db2f32309521a
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-16 12:12:24 -0400

    Set up Redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux"; /* code change */
+import recipeReducer from "./reducers/recipeReducer.js";
+
 import "./index.css";
 import App from "./App";
 
+const store = createStore(recipeReducer);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 
   document.getElementById("root")
 );
