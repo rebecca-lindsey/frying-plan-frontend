ad897fe2afa7e335b0425a2e223694cb1f9a0f42
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-16 12:13:49 -0400

    Add Redux webtools

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import recipeReducer from "./reducers/recipeReducer.js";
 import "./index.css";
 import App from "./App";
 
-const store = createStore(recipeReducer);
+const store = createStore(
+  recipeReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
