a4cbb377234dfe4edeaca382ba84f980e3b74f69
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-16 11:00:55 -0400

    Add navigation styling

--- src/App.js
+++ src/App.js
@@ -6,23 +6,23 @@ export default class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
+        <div className="App" id="app-container">
           <HeaderContainer />
+          <Switch>
+            <Route path="/users/:id/days">
+              <h1>Meal Plan Component Here</h1>
+            </Route>
+            <Route path="/recipes/new">
+              <h1>New Recipe Component Here</h1>
+            </Route>
+            <Route path="/recipes">
+              <h1>Recipes Component Here</h1>
+            </Route>
+            <Route path="/">
+              <h1>Welcome Page</h1>
+            </Route>
+          </Switch>
         </div>
-        <Switch>
-          <Route path="/users/:id/days">
-            <h1>Meal Plan Component Here</h1>
-          </Route>
-          <Route path="/recipes/new">
-            <h1>New Recipe Component Here</h1>
-          </Route>
-          <Route path="/recipes">
-            <h1>Recipes Component Here</h1>
-          </Route>
-          <Route path="/">
-            <h1>Welcome Page</h1>
-          </Route>
-        </Switch>
       </Router>
     );
   }
