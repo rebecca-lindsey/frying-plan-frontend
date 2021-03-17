065c98922055f2de1cd461c4209115874fd18bee
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-17 15:24:24 -0400

    Set up CreateLists

--- src/createrecipe/CreateRecipeContainer.js
+++ src/createrecipe/CreateRecipeContainer.js
@@ -4,9 +4,11 @@ import RecipeForm from "./RecipeForm";
 
 export default class CreateRecipe extends Component {
   render() {
+    console.log(this.props);
     return (
       <div id="create-recipe-container" className="main-container">
         <h2>Create Recipe</h2>
+
         <RecipeForm />
       </div>
     );
