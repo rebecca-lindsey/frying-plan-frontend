82bfd316e3151241c4e1db9aa5892f4160ff6da1
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-17 11:53:31 -0400

    Clean up recipeReducer, fill out dayReducer

--- src/reducers/recipeReducer.js
+++ src/reducers/recipeReducer.js
@@ -8,14 +8,12 @@ export default function recipeReducer(
   switch (action.type) {
     case "REQUEST_RECIPES":
       return {
-        ...state,
         recipes: [...state.recipes],
         loading: true,
       };
 
     case "ADD_RECIPES":
       return {
-        ...state,
         recipes: action.recipes,
         loading: false,
       };
