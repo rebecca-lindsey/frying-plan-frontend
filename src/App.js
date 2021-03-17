1458d9f06f49d61ddd540b0ac61ecfeb17bda1df
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-17 11:34:42 -0400

    Add MealPlanContainer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HeaderContainer from "./header/HeaderContainer";
 import HomeContainer from "./home/HomeContainer";
+import MealPlanContainer from "./mealplan/MealPlanContainer";
 import RecipeContainer from "./recipes/RecipeContainer";
 
 export default class App extends Component {
@@ -12,7 +13,7 @@ export default class App extends Component {
           <HeaderContainer />
           <Switch>
             <Route path="/users/:id/days">
-              <h1>Meal Plan Component Here</h1>
+              <MealPlanContainer />
             </Route>
             <Route path="/recipes/new">
               <h1>New Recipe Component Here</h1>
