61ab2b1176f4c0020e8fe71beb3ee11ebf52dccd
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-17 12:31:37 -0400

    Set up DayCard

--- src/mealplan/DayCard.js
+++ src/mealplan/DayCard.js
@@ -1,8 +1,22 @@
 function displayMeals(meals) {
-  console.log(meals);
-  return meals.map((meal) => <li></li>);
+  return meals.map((meal) => {
+    return (
+      <li>
+        {meal.name} - {meal.recipe.name}
+      </li>
+    );
+  });
 }
 
 export default function DayCard(props) {
-  return <div></div>;
+  return (
+    <div className="day-card" id={`recipe-${props.day.id}`}>
+      <h3>
+        <u>{props.day.name}</u>
+      </h3>
+      <p>
+        <b>Meals:</b> <ul>{displayMeals(props.day.meals)}</ul>
+      </p>
+    </div>
+  );
 }
