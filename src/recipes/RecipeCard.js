538eb0f32ea9b30154e403413f0c6260ae1be577
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-18 12:00:50 -0400

    Added keys to list in RecipeCard

--- src/recipes/RecipeCard.js
+++ src/recipes/RecipeCard.js
@@ -1,6 +1,7 @@
 function displayIngredients(ingredients) {
   return ingredients.map((ingredient) => (
-    <li>
+    <li key={`recipe_ingredient-${ingredient.id}`}>
+      {console.log(ingredient.id)}
       {ingredient.ingredient.name} - {ingredient.amount}
     </li>
   ));
@@ -13,17 +14,17 @@ export default function RecipeCard(props) {
         <u>{props.recipe.name}</u>
       </h3>
       <ul>
-        <li>
+        <li key={`recipe-${props.recipe.id}-category`}>
           <b>Category:</b> {props.recipe.category}
         </li>
-        <li>
+        <li key={`recipe-${props.recipe.id}-cuisine`}>
           <b>Cuisine:</b> {props.recipe.cuisine}
         </li>
-        <li>
+        <li key={`recipe-${props.recipe.id}-ingredients`}>
           <b>Ingredients:</b>
           <ul>{displayIngredients(props.recipe.recipe_ingredients)}</ul>
         </li>
-        <li>
+        <li key={`recipe-${props.recipe.id}-instructions`}>
           <b>Instructions:</b>
           <br />
           {props.recipe.instructions}
