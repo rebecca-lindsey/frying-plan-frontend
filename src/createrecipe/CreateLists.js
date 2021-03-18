9789fec2f455cccb7e23bb4310d1142124cf824d
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-18 11:27:12 -0400

    Corrected form formatting errors

--- src/createrecipe/CreateLists.js
+++ src/createrecipe/CreateLists.js
@@ -1,13 +1,13 @@
 function CategoryList(obj) {
   return (
-    <p>
+    <>
       <input type="text" name="category" list="category_list" />
       <datalist id="category_list">
         {obj.recipes.map((recipe) => (
           <option value={recipe.category}>{recipe.category}</option>
         ))}
       </datalist>
-    </p>
+    </>
   );
 }
 export { CategoryList };
