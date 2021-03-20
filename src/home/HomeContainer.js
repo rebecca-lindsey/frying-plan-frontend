5cd13d84514d5e18500007a3f6c6690af5fadddd
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-20 13:41:09 -0400

    Convert HomeContainer to function

--- src/home/HomeContainer.js
+++ src/home/HomeContainer.js
@@ -1,29 +1,26 @@
-import React, { Component } from "react";
 import "./home.css";
 import homeImage from "./home-image.jpg";
 
-export default class HomeContainer extends Component {
-  render() {
-    return (
-      <div id="home-container" className="main-container">
-        <h2>Welcome to Frying Plan!</h2>
-        <p>
-          Frying Plan is a meal planning app designed to make the weekly chore a
-          little more fun. Add your own recipes, use our classic recipes, or
-          find a new one on the web - the possibilities are endless!
-        </p>
-        <img src={homeImage} alt="Table with lots of food" id="home-image" />
-        <small>
-          Photo by{" "}
-          <a href="https://unsplash.com/@therachelstory?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-            Rachel Park
-          </a>{" "}
-          on{" "}
-          <a href="/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-            Unsplash
-          </a>
-        </small>
-      </div>
-    );
-  }
+export default function HomeContainer() {
+  return (
+    <div id="home-container" className="main-container">
+      <h2>Welcome to Frying Plan!</h2>
+      <p>
+        Frying Plan is a meal planning app designed to make the chore a little
+        more fun. Add your own recipes or use our classic recipes to create a
+        weekly schedule of yummy meals!
+      </p>
+      <img src={homeImage} alt="Table with lots of food" id="home-image" />
+      <small>
+        Photo by{" "}
+        <a href="https://unsplash.com/@therachelstory?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          Rachel Park
+        </a>{" "}
+        on{" "}
+        <a href="/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          Unsplash
+        </a>
+      </small>
+    </div>
+  );
 }
