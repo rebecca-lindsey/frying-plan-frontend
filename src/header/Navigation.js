60a2435c8f0f8d53ffae90ddaad1ab15f3cbc845
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-20 13:38:08 -0400

    Convert Navigation to function

--- src/header/Navigation.js
+++ src/header/Navigation.js
@@ -1,33 +1,30 @@
-import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
-export default class Navigation extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <NavLink exact to="/" activeClassName="active">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={`/users/1/days`} activeClassName="active">
-              Meal Plan
-            </NavLink>
-          </li>
-          <li>
-            <NavLink exact to="/recipes" activeClassName="active">
-              All Recipes
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/recipes/new" activeClassName="active">
-              New Recipe
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+export default function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink exact to="/" activeClassName="active">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={`/users/1/days`} activeClassName="active">
+            Meal Plan
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/recipes" activeClassName="active">
+            All Recipes
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/recipes/new" activeClassName="active">
+            New Recipe
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
