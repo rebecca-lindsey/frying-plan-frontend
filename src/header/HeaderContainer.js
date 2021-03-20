0a5c8955951dd473bf5abc8917dc41e8920056dd
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-20 13:38:51 -0400

    Convert HeaderContainer to function

--- src/header/HeaderContainer.js
+++ src/header/HeaderContainer.js
@@ -1,14 +1,11 @@
-import React, { Component } from "react";
 import Navigation from "./Navigation";
 import "./header.css";
 
-export default class HeaderContainer extends Component {
-  render() {
-    return (
-      <div id="header-container">
-        <h1>Frying Plan</h1>
-        <Navigation />
-      </div>
-    );
-  }
+export default function HeaderContainer() {
+  return (
+    <div id="header-container">
+      <h1>Frying Plan</h1>
+      <Navigation />
+    </div>
+  );
 }
