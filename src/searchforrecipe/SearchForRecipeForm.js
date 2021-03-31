b721d61913dd27bd58824be95f36aacbcc2d7eb0
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-31 17:37:27 -0400

    Change SearchForRecipeForm to functional component

--- src/searchforrecipe/SearchForRecipeForm.js
+++ src/searchforrecipe/SearchForRecipeForm.js
@@ -1,20 +1,16 @@
-import React, { Component } from "react";
-
-export default class SearchForRecipeForm extends Component {
-  render() {
-    return (
-      <form id="search-for-recipe-form">
-        <p>
-          <label htmlFor="searchTerm">Name of Meal: </label>
-          <input
-            type="text"
-            name="searchTerm"
-            value={this.props.searchTerm}
-            onChange={this.props.handleChange}
-          />
-        </p>
-        <input type="submit" />
-      </form>
-    );
-  }
+export default function SearchForRecipeForm(props) {
+  return (
+    <form id="search-for-recipe-form">
+      <p>
+        <label htmlFor="searchTerm">Name of Meal: </label>
+        <input
+          type="text"
+          name="searchTerm"
+          value={props.searchTerm}
+          onChange={props.handleChange}
+        />
+      </p>
+      <input type="submit" />
+    </form>
+  );
 }
