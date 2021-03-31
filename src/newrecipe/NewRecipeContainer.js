fa098c4bf001e12ba9bf22863f872e8254fba057
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-31 16:52:02 -0400

    Set up handleChange for NewRecipeContainer

--- src/newrecipe/NewRecipeContainer.js
+++ src/newrecipe/NewRecipeContainer.js
@@ -7,18 +7,40 @@ export default class NewRecipeContainer extends Component {
     display: "create-recipe",
   };
 
+  handleChange = (e) => {
+    if (
+      e.target.value === "create-recipe" ||
+      e.target.value === "search-for-recipe"
+    ) {
+      this.setState({
+        display: e.target.value,
+      });
+    }
+  };
+
   render() {
     return (
       <div id="new-recipe-container" className="main-container">
         <h2>New Recipe</h2>
         <form id="new-recipe-form">
           <p>
-            <input type="radio" name="display" value="create-recipe" checked />
-            <label for="create-recipe">Create a new recipe</label>
+            <input
+              type="radio"
+              name="display"
+              value="create-recipe"
+              onChange={this.handleChange}
+              defaultChecked
+            />
+            <label htmlFor="create-recipe">Create a new recipe</label>
           </p>
           <p>
-            <input type="radio" name="display" value="search-for-recipe" />
-            <label for="search-for-recipe">Search for a new recipe</label>
+            <input
+              type="radio"
+              name="display"
+              value="search-for-recipe"
+              onChange={this.handleChange}
+            />
+            <label htmlFor="search-for-recipe">Search for a new recipe</label>
           </p>
         </form>
 
